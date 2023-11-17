36174aac48cb639b660ac06a3d888176a119f7f8
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-17 13:21:30 +0900

    [level unrated] Title: A 강조하기, Time: 0.09 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/unrated/181874. A 강조하기/A 강조하기.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(String myString) {
+        /*
         StringBuilder sb = new StringBuilder();
         for(char c : myString.toCharArray()){
             String str = String.valueOf(c);
             str = str.equals("a") || str.equals("A") ? str.toUpperCase() : str.toLowerCase();
             sb.append(str);
         }
-        return sb.toString();
+        */
+        return myString.toLowerCase().replaceAll("a", "A");
     }
 }
