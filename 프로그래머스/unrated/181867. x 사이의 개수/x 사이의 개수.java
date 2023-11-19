c4ff7830cbce4a3fb58867ecc226f06fa2b892c2
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-19 20:38:12 +0900

    [level unrated] Title: x 사이의 개수, Time: 13.72 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
+++ 프로그래머스/unrated/181867. x 사이의 개수/x 사이의 개수.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 class Solution {
     public int[] solution(String myString) {
+        /*
         ArrayList<Integer> arr = new ArrayList<>();
         int cnt = 0;
         for(int i=0; i<myString.length(); i++){
@@ -14,6 +16,10 @@ class Solution {
             if(i+1 == myString.length()) arr.add(cnt);
         }
         int[] answer = arr.stream().mapToInt(x->x).toArray();
+        */
+        int[] answer = Arrays.stream(myString.split("x", myString.length()))
+            .mapToInt(x -> x.length())
+            .toArray();
         
         return answer;
     }
