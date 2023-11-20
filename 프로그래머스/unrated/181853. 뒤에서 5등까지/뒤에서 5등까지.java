37fd7dcac1531af944dc6a7a1c8a0451800d06f8
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-20 17:11:25 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 0.35 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -2,12 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] num_list) {
+        /*
         int[] answer = new int[5];
         Arrays.sort(num_list);
         for(int i=0; i<answer.length; i++){
             answer[i] = num_list[i];
         }
+        */
+        Arrays.sort(num_list);
         
-        return answer;
+        
+        return Arrays.copyOfRange(num_list, 0, 5);
     }
 }
