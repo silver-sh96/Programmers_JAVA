d18e3296f89b8563c692ecd048d831efa19e26a1
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-22 10:12:11 +0900

    [level unrated] Title: 날짜 비교하기, Time: 0.78 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,5 +1,8 @@
+import java.time.LocalDate;
+
 class Solution {
     public int solution(int[] date1, int[] date2) {
+        /*
         int answer = 0;
         for(int i=0; i<date1.length; i++){
             if(date1[i] < date2[i]){
@@ -10,6 +13,10 @@ class Solution {
                 break;
             }
         }
+        */
+        LocalDate LDate1 = LocalDate.of(date1[0],date1[1],date1[2]);
+        LocalDate LDate2 = LocalDate.of(date2[0],date2[1],date2[2]);
+        int answer = LDate1.isBefore(LDate2) ? 1 : 0;
         
         return answer;
     }
