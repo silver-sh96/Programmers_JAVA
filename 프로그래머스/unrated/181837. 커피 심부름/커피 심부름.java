477476a5512140ae9d0042304f7d277b6b4a3784
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-23 13:23:09 +0900

    [level unrated] Title: 커피 심부름, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String[] order) {
+        int answer = 0;
+        for(String str : order){
+            if(str.contains("americano")) answer += 4500;
+            else if(str.contains("latte")) answer += 5000;
+            else if(str.contains("any")) answer += 4500;
+        }
+        
+        return answer;
+    }
+}
