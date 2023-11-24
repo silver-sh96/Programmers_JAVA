9cff08d3ca62e53e79302b330ceb68f1e0036846
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-24 17:19:36 +0900

    [level unrated] Title: l로 만들기, Time: 18.19 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,11 +1,13 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
+        /*
         StringBuilder sb = new StringBuilder();
         for(char c : myString.toCharArray()){
             if(c < 'l') sb.append(String.valueOf('l'));
             else sb.append(String.valueOf(c));
         }
         return sb.toString();
+        */
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
