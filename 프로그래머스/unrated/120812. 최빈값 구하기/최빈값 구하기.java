dbfbb79687720c402e7daec68fc5db6a9d1de4c2
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-28 15:20:49 +0900

    [level unrated] Title: 최빈값 구하기, Time: 0.14 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,7 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] array) {
-        int answer = 0;
+        int answer = 0, max = 0;
+        Map<Integer, Integer> arrMap = new HashMap<>();
+        for(int num : array){
+            int cnt = arrMap.getOrDefault(num, 0) + 1;
+            if(cnt > max){
+                max = cnt;
+                answer = num;
+            }else if(cnt == max){
+                answer = -1;
+            }
+            arrMap.put(num, cnt);
+        }
+        /*
         Arrays.sort(array);
         int[] cnt = new int[array[array.length-1]+1];
         
@@ -18,6 +30,7 @@ class Solution {
                 answer = -1;
             }
         }
+        */
         
         return answer;
     }
