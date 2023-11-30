d2aa960990cc2d3370dfccc8675866a847106f63
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-11-30 15:29:59 +0900

    [level unrated] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/unrated/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -2,11 +2,11 @@ class Solution {
     public int solution(int price) {
         int answer = 0;
         if(price >= 100000 && price < 300000){
-            answer = (int) Math.floor(Double.valueOf(price) * 0.95);
+            answer = (int) (price * 0.95);
         }else if(price >= 300000 && price < 500000){
-            answer = (int) Math.floor(Double.valueOf(price) * 0.90);
+            answer = (int) (price * 0.90);
         }else if(price >= 500000){
-            answer = (int) Math.floor(Double.valueOf(price) * 0.80);
+            answer = (int) (price * 0.80);
         }else if(price < 100000){
             answer = price;
         }
