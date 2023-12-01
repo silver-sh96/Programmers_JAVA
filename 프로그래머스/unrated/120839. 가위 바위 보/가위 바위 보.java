35c0907cb39041bb0de9516601e808f086d16f26
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-01 15:17:01 +0900

    [level unrated] Title: 가위 바위 보, Time: 0.07 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder sb = new StringBuilder();
+        for(char c : rsp.toCharArray()){
+            if(c == '2') sb.append("0");
+            if(c == '0') sb.append("5");
+            if(c == '5') sb.append("2");
+        }
+        
+        return sb.toString();
+    }
+}
