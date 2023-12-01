fdbda9f7497eeb81c476ac49c214021b34e2879e
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-01 18:01:22 +0900

    [level unrated] Title: 모음 제거, Time: 0.11 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/unrated/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,15 @@
 class Solution {
     public String solution(String my_string) {
+        /*
         StringBuilder sb = new StringBuilder();
         String vowel = "aeiou";
         for(char c : my_string.toCharArray()){
             if(!(vowel.contains(String.valueOf(c)))) sb.append(String.valueOf(c));
         }
         return sb.toString();
+        */
+        String answer = my_string.replaceAll("[aeiou]", "");
+        
+        return answer;
     }
 }
