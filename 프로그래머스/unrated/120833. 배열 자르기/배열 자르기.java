11aa30c95d5c3eb5d73ef0297df9ea0f94f3604b
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-01 10:08:41 +0900

    [level unrated] Title: 배열 자르기, Time: 2.08 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/unrated/120833. 배열 자르기/배열 자르기.java
@@ -2,7 +2,8 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
+        //int[] answer = Arrays.copyOfRange(numbers, num1, num2+1);
+        int[] answer = Arrays.stream(numbers, num1, num2+1).toArray();
         return answer;
     }
 }
