d109b998c598a0e6572d2828c4b8d7a22dcaca4b
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-06 11:31:28 +0900

    [level unrated] Title: 공 던지기, Time: 0.03 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
@@ -1,11 +1,13 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
+        int answer = numbers[((k-1)*2)%numbers.length];
+        /*
         for(int i=1, n=0; i<=k; i++){
             if(n < numbers.length) answer = numbers[n];
             else answer = numbers[n%numbers.length];
             n += 2;
         }
+        */
         return answer;
     }
 }
