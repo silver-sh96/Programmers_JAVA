24f232b4e821929d4119b665dfbe9551387dd2af
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-06 16:44:24 +0900

    [level unrated] Title: 문자열 정렬하기 (1), Time: 3.95 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/unrated/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,12 +2,15 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String my_string) {
+        /*
         ArrayList<Integer> arr = new ArrayList<>();
         for(char c : my_string.replaceAll("[^0-9]", "").toCharArray()){
             arr.add(Character.getNumericValue(c));
         }
-        int[] answer = arr.stream().mapToInt(x->x).toArray();
-        Arrays.sort(answer);
+        int[] answer = arr.stream().sorted().mapToInt(x->x).toArray();
+        */
+        int[] answer = Arrays.stream(my_string.replaceAll("[^0-9]", "").split(""))
+                             .sorted().mapToInt(Integer::parseInt).toArray();
         return answer;
     }
 }
