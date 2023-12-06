978d51836f56e7a5d47ff9e790bfc79524332766
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-06 17:55:46 +0900

    [level unrated] Title: 문자열 정렬하기 (2), Time: 0.39 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/unrated/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -4,8 +4,8 @@ class Solution {
     public String solution(String my_string) {
         char[] arr = my_string.toLowerCase().toCharArray();
         Arrays.sort(arr);
-        String answer = new StringBuilder(new String(arr)).toString();
+        //String answer = new StringBuilder(new String(arr)).toString();
         
-        return answer;
+        return new String(arr);
     }
 }
