9b39fc5e2908604e5c62eef73e1cd30949ee410b
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-06 14:57:48 +0900

    [level unrated] Title: n의 배수 고르기, Time: 3.10 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/unrated/120905. n의 배수 고르기/n의 배수 고르기.java
+++ 프로그래머스/unrated/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -2,11 +2,14 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, int[] numlist) {
+        /*
         ArrayList<Integer> arr = new ArrayList<>();
         for(int num : numlist){
             if(num%n == 0) arr.add(num);
         }
         int[] answer = arr.stream().mapToInt(x->x).toArray();
-        return answer;
+        */
+        
+        return Arrays.stream(numlist).filter(x->x%n==0).toArray();
     }
 }
