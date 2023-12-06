b311251e846430e9fe91986522e6131eb65d228f
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-06 14:54:25 +0900

    [level unrated] Title: 세균 증식, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
+++ 프로그래머스/unrated/120910. 세균 증식/세균 증식.java
@@ -1,9 +1,11 @@
 class Solution {
     public int solution(int n, int t) {
-        int answer = n;
+        int answer = n * (int)Math.pow(2, t);
+        /*
         for(int i=1; i<=t; i++){
             answer *= 2;
         }
+        */
         return answer;
     }
 }
