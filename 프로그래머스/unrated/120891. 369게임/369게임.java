b342efb5c1b9ac8d3e14490ad5e8e5792ef8ac73
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-06 17:45:24 +0900

    [level unrated] Title: 369게임, Time: 1.07 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/unrated/120891. 369게임/369게임.java
+++ 프로그래머스/unrated/120891. 369게임/369게임.java
@@ -1,9 +1,16 @@
+import java.util.*;
+
 class Solution {
     public int solution(int order) {
         int answer = 0;
+        /*
         for(String str : String.valueOf(order).split("")){
             if(str.equals("3") || str.equals("6") || str.equals("9")) answer++;
         }
+        */
+        answer = (int)Arrays.stream(String.valueOf(order).split(""))
+            .map(Integer::parseInt).filter(i -> i==3 || i==6 || i==9).count();
+        
         return answer;
     }
 }
