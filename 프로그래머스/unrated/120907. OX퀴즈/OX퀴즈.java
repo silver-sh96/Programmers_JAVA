f7d0680a9ef565f49094e8dc699d1fbe2bca962e
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-12 10:39:41 +0900

    [level unrated] Title: OX퀴즈, Time: 0.17 ms, Memory: 85.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/unrated/120907. OX퀴즈/OX퀴즈.java
@@ -3,7 +3,9 @@ class Solution {
         String[] answer = new String[quiz.length];
         for(int i=0; i<quiz.length; i++){
             String[] exp = quiz[i].split(" ");
+            // 식의 첫 숫자 대입
             int num = Integer.parseInt(quiz[i].split(" ")[0]);
+            // 식의 결과 값 대입
             int result = Integer.parseInt(quiz[i].split(" ")[quiz[i].split(" ").length-1]);
             for(int j=1; j<exp.length; j++){
                 if(exp[j].equals("+")) num += Integer.parseInt(exp[j+1]);
