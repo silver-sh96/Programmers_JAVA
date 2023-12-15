a6128a51abbcef3b2f41d0bc21f4663f65500d6a
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-15 09:46:52 +0900

    [level unrated] Title: 평행, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120875. 평행/평행.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[][] dots) {
+        int answer = 0;
+        int x1 = dots[0][0], y1 = dots[0][1];
+        int x2 = dots[1][0], y2 = dots[1][1];
+        int x3 = dots[2][0], y3 = dots[2][1];
+        int x4 = dots[3][0], y4 = dots[3][1];
+        
+        if((double)(y2-y1)/(x2-x1) == (double)(y4-y3)/(x4-x3)){
+            answer = 1;
+        }else if((double)(y3-y1)/(x3-x1) == (double)(y4-y2)/(x4-x2)){
+            answer = 1;
+        }else if((double)(y4-y1)/(x4-x1) == (double)(y3-y2)/(x3-x2)){
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
