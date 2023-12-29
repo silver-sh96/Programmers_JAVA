be29ab8752bd5c57a8adfffe10aed360a6c8a157
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-29 14:50:51 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.14 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(String str : String.valueOf(n).split("")){
+            answer += Integer.parseInt(str);
+        }
+
+        return answer;
+    }
+}
