168297b24f8dd9ae0694768fa441e26aa7550d76
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2023-12-29 17:18:36 +0900

    [level 1] Title: 과일 장수, Time: 0.51 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -4,18 +4,19 @@ class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0, len = score.length;
         Arrays.sort(score);
+        
+        for(int i=len; i>=m; i-=m){
+            answer += score[i-m] * m;
+        }
+        /*
         int[][] result = new int[len/m][m];
         
-        if(result.length == 1){
-            result[0] = Arrays.copyOfRange(score, len - m, len);
-            answer = result[0][0] * m;
-        }else{
-            for(int i=0, idx=len; i<result.length; i++){
-                result[i] = Arrays.copyOfRange(score, len - m, len);
-                answer += result[i][0] * m;
-                len -= m;
-            }    
+        for(int i=0, idx=len; i<result.length; i++){
+            result[i] = Arrays.copyOfRange(score, len - m, len);
+            answer += result[i][0] * m;
+            len -= m;
         }
+        */
         
         return answer;
     }
