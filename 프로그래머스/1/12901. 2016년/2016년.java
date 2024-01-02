1d46e22d869be8e123d5609c6d9e6c35dd4b53c4
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2024-01-02 10:20:27 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -2,7 +2,18 @@ import java.time.*;
 
 class Solution {
     public String solution(int a, int b) {
-        String answer = LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
+        String answer = "";
+        //String answer = LocalDate.of(2016, a, b).getDayOfWeek().toString().substring(0, 3);
+        String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int allDay = 0;
+        
+        for(int i=0; i<a-1; i++){
+            allDay += date[i];
+        }
+        allDay += (b - 1);
+        answer = day[allDay % 7];
+        
         return answer;
     }
 }
