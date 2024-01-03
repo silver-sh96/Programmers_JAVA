bddd292b342dc96b9bf231daafd4ac6c57df61c7
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2024-01-03 09:47:05 +0900

    [level 1] Title: 덧칠하기, Time: 48.22 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<section.length; j++){
+                if(section[j] == i){
+                    answer++;
+                    i += m;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
