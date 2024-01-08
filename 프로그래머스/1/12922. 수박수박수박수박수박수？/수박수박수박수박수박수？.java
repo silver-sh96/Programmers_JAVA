5ad66de7aec8f5b48da353f1b88139482953d3d3
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2024-01-08 12:07:48 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.09 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,8 +2,7 @@ class Solution {
     public String solution(int n) {
         StringBuilder sb = new StringBuilder();
         while(sb.length() < n){
-            if(sb.length() % 2 == 0) sb.append("수");
-            else sb.append("박");
+            sb.append(sb.length() % 2 == 0 ? "수" : "박");
         }
         
         return sb.toString();
