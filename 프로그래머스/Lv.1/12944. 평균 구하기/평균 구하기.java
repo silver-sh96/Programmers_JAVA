6e0d75da3d6638376ad00e1193c1d77d0da7867b
eun seung ho <107014838+silver-sh96@users.noreply.github.com>
2024-01-12 14:20:43 +0900

    [level Lv.1] Title: 평균 구하기, Time: 2.80 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12944. 평균 구하기/평균 구하기.java
+++ 프로그래머스/Lv.1/12944. 평균 구하기/평균 구하기.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 class Solution {
     public double solution(int[] arr) {
-        
+        /*
         int sum = Arrays.stream(arr).sum();
         double answer = (double) sum / arr.length;
         
         return answer;
+        */
+        return Arrays.stream(arr).average().getAsDouble();
     }
 }
